32c4e4e5e5e58718adeec65ca318c45f2cabd3c5
dvxgit-jsoriano <jsoriano@diavox.net>
2021-04-11 01:29:05 +0800

    Added migration create notification and status to orders

---
+++ database/migrations/2021_04_10_171216_create_notifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('title')->default('');
+            $table->string('message')->default('');
+            $table->boolean('read')->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
